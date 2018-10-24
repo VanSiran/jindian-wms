25e61edf1caa8ee6576ad8ef949c3d833a6fd9e3
贾奇 <vipjiaqi@gmail.com>
2018-10-25 00:35:19 +0800

    接口修改2

--- controllers/interface.py
+++ controllers/interface.py
@@ -4,5 +4,5 @@ from odoo.http import request
 class Main(http.Controller):
     @http.route('/wms/json/list', type='json', auth='none')
     def list_geti(self):
-        records = request.env['wms.geti'].sudo().list_geti(0, [‘电务段’，‘京沪高铁车间’，‘沧州西站工区’]，[‘轨道电路’，‘ZPW-2000K’])
+        records = request.env['wms.geti'].sudo().list_geti(0, ['电务段'，'京沪高铁车间'，'沧州西站工区'], ['轨道电路'，'ZPW-2000K'])
         return records
